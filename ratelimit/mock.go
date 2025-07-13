2ab5dd25475cef5517fea6375603de9beb6aa4f8
Benjamin Bengfort <benjamin@bengfort.com>
2025-07-12 19:02:05 -0500

    fix race condition

--- ratelimit/mock.go
+++ ratelimit/mock.go
@@ -1,6 +1,7 @@
 package ratelimit
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,7 @@ import (
 
 // Mock implements the Limiter interface for package testing purposes.
 type Mock struct {
+	sync.RWMutex
 	conf      Config
 	calls     map[string]int
 	OnAllow   func(c *gin.Context) (bool, Headers)
@@ -16,7 +18,9 @@ type Mock struct {
 }
 
 func (r *Mock) Allow(c *gin.Context) (bool, Headers) {
-	r.calls["Allow"]++
+	r.incr("Allow")
+	r.RLock()
+	defer r.RUnlock()
 	if r.OnAllow != nil {
 		return r.OnAllow(c)
 	}
@@ -24,13 +28,17 @@ func (r *Mock) Allow(c *gin.Context) (bool, Headers) {
 }
 
 func (r *Mock) Cleanup() {
-	r.calls["Cleanup"]++
+	r.incr("Cleanup")
+	r.RLock()
+	defer r.RUnlock()
 	if r.OnCleanup != nil {
 		r.OnCleanup()
 	}
 }
 
 func (r *Mock) Reset() {
+	r.Lock()
+	defer r.Unlock()
 	r.calls = nil
 	r.calls = make(map[string]int)
 	r.OnAllow = nil
@@ -39,5 +47,17 @@ func (r *Mock) Reset() {
 
 func (r *Mock) AssertCalls(t *testing.T, method string, expected int) {
 	t.Helper()
+
+	r.RLock()
+	defer r.RUnlock()
 	require.Equal(t, expected, r.calls[method], "expected %d calls to %s, got %d", expected, method, r.calls[method])
+}
+
+func (r *Mock) incr(method string) {
+	r.Lock()
+	defer r.Unlock()
+	if r.calls == nil {
+		r.calls = make(map[string]int)
+	}
+	r.calls[method]++
 }
